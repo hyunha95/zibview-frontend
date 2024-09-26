f7bfda86a5f8a5f5bcc0825deba3096e32aed572
hyunha <nohyunha95@gmail.com>
2024-09-26 09:03:14 +0900

    fixed import

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,6 +1,6 @@
 "use client";
 import posthog from "posthog-js";
-import { PostHogProvider } from "posthog-react";
+import { PostHogProvider } from "posthog/react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
