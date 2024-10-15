b547d4bdce899794203f8c0142b60ff640f01138
hyunha <nohyunha95@gmail.com>
2024-10-15 09:00:36 +0900

    add carousel and hovercard

--- src/components/NaverMapLeftSide.tsx
+++ src/components/NaverMapLeftSide.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import Link from "next/link";
-
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import { AlignJustify, Bell, Search } from "lucide-react";
+import { useRef } from "react";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function NaverMapLeftSide() {
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
+
   return (
     <div className="p-4">
       <div className="flex justify-between mb-4">
@@ -25,60 +35,41 @@ export default function NaverMapLeftSide() {
         </Link>
       </div>
 
-      <div className="flex items-center gap-x-2 border p-1 rounded-md">
-        <Link
-          href="#"
-          className="bg-gradient-to-r from-pink-500 to-orange-500 inline-block px-2 py-2 rounded-md min-w-fit"
-        >
-          <h2 className="text font-semibold text-white leading-none">실시간</h2>
-        </Link>
-        <div className="grid grid-cols-10 w-full">
-          <span className="col-span-1 font-bold text-orange-600 text-center">
-            1
-          </span>
-          <span className="col-span-7 font-semibold">서울 강남구 대치동</span>
-          <span className="col-span-2 text-center font-semibold">1,500</span>
-        </div>
-      </div>
+      <Carousel
+        plugins={[plugin.current]}
+        className="w-full max-w-xs"
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+        orientation="vertical"
+      >
+        <CarouselContent className="h-16">
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="flex items-center gap-x-2 border p-1 rounded-md">
+                <Link
+                  href="#"
+                  className="bg-gradient-to-r from-pink-500 to-orange-500 inline-block px-2 py-2 rounded-md min-w-fit"
+                >
+                  <h2 className="text font-semibold text-white leading-none">
+                    실시간
+                  </h2>
+                </Link>
+                <div className="grid grid-cols-10 w-full">
+                  <span className="col-span-1 font-bold text-orange-600 text-center">
+                    1
+                  </span>
+                  <span className="col-span-7 font-semibold">
+                    서울 강남구 대치동
+                  </span>
+                  <span className="col-span-2 text-center font-semibold">
+                    1,500
+                  </span>
+                </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </div>
   );
-}
-
-{
-  /* <button
-        onClick={() => setActive("APARTMENT")}
-        className={`flex flex-col items-center gap-0.5 py-1 rounded-md drop-shadow-lg shadow-inner ${
-          active === "APARTMENT" ? "bg-orange-500/80" : "bg-white"
-        }`}
-      >
-        <Building2
-          size={20}
-          className={`${active === "APARTMENT" && "text-white"}`}
-        />
-        <span
-          className={`text-xs font-semibold ${
-            active === "APARTMENT" && "text-white"
-          }`}
-        >
-          아파트
-        </span>
-      </button>
-      <button
-        onClick={() => setActive("OFFICETEL")}
-        className={`flex flex-col items-center gap-0.5 py-1 rounded-md drop-shadow-lg shadow-inner ${
-          active === "OFFICETEL" ? "bg-orange-500/80" : "bg-white"
-        }`}
-      >
-        <Building
-          size={20}
-          className={`${active === "OFFICETEL" && "text-white"}`}
-        />
-        <span
-          className={`text-xs font-semibold ${
-            active === "OFFICETEL" && "text-white"
-          }`}
-        >
-          오피스텔
-        </span>
-      </button> */
 }
