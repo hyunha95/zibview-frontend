ebb5ac73e060269a52b6bc85021429797a32687c
nohyuha <nohyunha95@gmail.com>
2024-11-04 22:23:54 +0900

    server url 환경 변수 처리

--- src/app/@map/page.tsx
+++ src/app/@map/page.tsx
@@ -3,7 +3,7 @@ import NaverMap from "@/components/NaverMap";
 export default async function MapPage() {
   const fetchCookies = async () => {
     const response = await fetch(
-      "http://localhost:8080/api/auth/anonymous/cookies",
+      process.env.NEXT_PUBLIC_SERVER_URL + "/api/auth/anonymous/cookies",
       { cache: "no-store" }
     );
     const body = await response.json();
