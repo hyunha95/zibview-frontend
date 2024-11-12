2e9b8f7ecbcd5de67af8bb3ff404a4fd9013b937
nohyuha <nohyunha95@gmail.com>
2024-11-13 01:45:06 +0900

    feat: 뉴스 컴포넌트 수정

--- src/app/@info/components/News.tsx
+++ src/app/@info/components/News.tsx
@@ -4,39 +4,44 @@ import React from "react";
 
 export default function News() {
   return (
-    <div className="w-full border rounded-md">
-      <Link href="#" className="inline-block p-3 space-y-2">
-        <h4 className="text-sm font-semibold leading-4">
-          왜 우리집은 안 오르지?"…서울 집값, '여기'만 오른다[복덕방기자들]
-        </h4>
-        <div className="flex items-center h-3 space-x-2 text-sm">
-          <span className="text-gray-500">이데일리</span>
-          <Separator orientation="vertical" />
-          <span className="text-gray-500">18일 전</span>
-        </div>
+    <>
+      <div className="w-full border rounded-md">
+        <Link href="#" className="inline-block p-3 space-y-2">
+          <h4 className="text-sm font-semibold leading-4">
+            왜 우리집은 안 오르지?"…서울 집값, '여기'만 오른다[복덕방기자들]
+          </h4>
+          <div className="flex items-center h-3 space-x-2 text-sm">
+            <span className="text-gray-500 text-xs">이데일리</span>
+            <Separator orientation="vertical" />
+            <span className="text-gray-500 text-xs">18일 전</span>
+          </div>
+        </Link>
+        <Separator className="w-11/12 mx-auto" />
+        <Link href="#" className="inline-block p-3 space-y-2">
+          <h4 className="text-sm font-semibold leading-4">
+            왜 우리집은 안 오르지?"…서울 집값, '여기'만 오른다[복덕방기자들]
+          </h4>
+          <div className="flex items-center h-3 space-x-2 text-sm">
+            <span className="text-gray-500 text-xs">이데일리</span>
+            <Separator orientation="vertical" />
+            <span className="text-gray-500 text-xs">18일 전</span>
+          </div>
+        </Link>
+        <Separator className="w-11/12 mx-auto" />
+        <Link href="#" className="inline-block p-3 space-y-2">
+          <h4 className="text-sm font-semibold leading-4">
+            왜 우리집은 안 오르지?"…서울 집값, '여기'만 오른다[복덕방기자들]
+          </h4>
+          <div className="flex items-center h-3 space-x-2 text-sm">
+            <span className="text-gray-500 text-xs">이데일리</span>
+            <Separator orientation="vertical" />
+            <span className="text-gray-500 text-xs">18일 전</span>
+          </div>
+        </Link>
+      </div>
+      <Link href="#" className="px-4 py-1 mx-auto text-sm border rounded-full">
+        경기도 판교 뉴스 더보기
       </Link>
-      <Separator className="w-11/12 mx-auto" />
-      <Link href="#" className="inline-block p-3 space-y-2">
-        <h4 className="text-sm font-semibold leading-4">
-          왜 우리집은 안 오르지?"…서울 집값, '여기'만 오른다[복덕방기자들]
-        </h4>
-        <div className="flex items-center h-3 space-x-2 text-sm">
-          <span className="text-gray-500">이데일리</span>
-          <Separator orientation="vertical" />
-          <span className="text-gray-500">18일 전</span>
-        </div>
-      </Link>
-      <Separator className="w-11/12 mx-auto" />
-      <Link href="#" className="inline-block p-3 space-y-2">
-        <h4 className="text-sm font-semibold leading-4">
-          왜 우리집은 안 오르지?"…서울 집값, '여기'만 오른다[복덕방기자들]
-        </h4>
-        <div className="flex items-center h-3 space-x-2 text-sm">
-          <span className="text-gray-500">이데일리</span>
-          <Separator orientation="vertical" />
-          <span className="text-gray-500">18일 전</span>
-        </div>
-      </Link>
-    </div>
+    </>
   );
 }
